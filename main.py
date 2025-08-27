76a50cb68dbadc7fa5cc1dab85bf9754740b5d4c
limitedink <54423958+limitedink@users.noreply.github.com>
2025-08-27 11:16:18 +1000

    ADD sort_report function to stats.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count, char_count
+from stats import word_count, char_count, sort_report 
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
@@ -9,7 +9,14 @@ def main():
     outputtext = get_book_text("./books/frankenstein.txt")
     wc = word_count(outputtext)
     cc = char_count(outputtext)
-    print(f"{wc} words found in the document")
-    print(cc)
+    report = ""
+    report = sort_report(cc)
 
+    print(f"""============ BOOKBOT ============
+Analyzing book found at books/frankenstein.txt...
+----------- Word Count ----------
+Found {wc} total words
+--------- Character Count -------
+{report}
+============= END ===============""")
 main()
--- stats.py
+++ stats.py
@@ -10,9 +10,25 @@ def char_count(text):
     char_counts = {}
     text = text.lower()
     for char in text:
+        if not char.isalpha():
+            continue
         if char in char_counts:
             char_counts[char] += 1
         else:
             char_counts[char] = 1
     return char_counts
+
+def sort_report(char_counts):
+    report_list = list(char_counts.items())
+    # for char, count in char_counts.items():
+    #report_list.append({char: count})
+
+    def sort_by_count(item):
+        return item[1]
+
+    report_list.sort(reverse = True, key=sort_by_count)
+    return "\n".join(f"{char}: {count}" for char, count in report_list)
+
+
+
       
